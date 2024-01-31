288c3f1ace4b6e7460621761a9a4dfa945164d98
Hugo Frisk <hugo@interns.teenage.engineering>
2024-01-31 18:04:04 +0100

    restructure

--- models.py
+++ models.py
@@ -1,21 +1,129 @@
+import timm
+import torch
 from torch import nn
 
-# return the untrained patch description model (PDN)
-def get_pdn(channels: int = 512) -> nn.Sequential:
-    return nn.Sequential(
-        nn.Conv2d(in_channels=3, out_channels=128, kernel_size=4, stride=1, padding=3),
-        nn.ReLU(inplace=True),
-        nn.AvgPool2d(kernel_size=2, stride=2, padding=1),
-        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=1, padding=3),
-        nn.ReLU(inplace=True),
-        nn.AvgPool2d(kernel_size=2, stride=2, padding=1),
-        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(in_channels=256, out_channels=channels, kernel_size=4, stride=1, padding=0)
-    )
 
-# return the untrained autoencoder
-def get_autencoder(channels: int = 256) -> nn.Sequential:
-    return nn.Sequential(
-        
-    )
+class ChoppedWideResNet(nn.Module):
+    def __init__(self, channels: int, layer_to_extract_from: str, layer_index: int = -1) -> None:
+        super().__init__()
+        self._backbone = timm.create_model("wide_resnet101_2", pretrained=True)
+        self._output = None
+        self._channels = channels
+
+        def forward_hook(m, inputs: torch.Tensor, outputs: torch.Tensor) -> None:
+            self._output = outputs
+            raise self.LayerReachedException
+
+        for layer in self._backbone.named_children():
+            if layer[0] == layer_to_extract_from:
+                layer[1][layer_index].register_forward_hook(forward_hook)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        with torch.no_grad():
+            try:
+                self._backbone.forward(input)
+            except self.LayerReachedException:
+                pass
+        return self._output[:, : self._channels, :, :]
+
+    class LayerReachedException(Exception):
+        pass
+
+
+class PatchDescriptionNetwork(nn.Module):
+    def __init__(self, channels: int) -> None:
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=4, stride=1, padding=3)
+        self.avgpool_1 = nn.AvgPool2d(kernel_size=2, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=1, padding=3)
+        self.avgpool2 = nn.AvgPool2d(kernel_size=2, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=256, out_channels=channels, kernel_size=4, stride=1, padding=0)
+
+    def forward(self, x) -> torch.Tensor:
+        x = self.conv1(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.avgpool_1(x)
+        x = self.conv2(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.avgpool2(x)
+        x = self.conv3(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.conv4(x)
+        return x
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self, channels: int) -> None:
+        super().__init__()
+
+        # encoding
+        self.enc_conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=4, stride=2, padding=1)
+        self.enc_conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=4, stride=2, padding=1)
+        self.enc_conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1)
+        self.enc_conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=2, padding=1)
+        self.enc_conv5 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=2, padding=1)
+        self.enc_conv6 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=8, stride=1, padding=0)
+
+        # decoding
+        self.dec_conv1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2)
+        self.dec_dropout1 = nn.Dropout(p=0.2)
+        self.dec_conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2)
+        self.dec_dropout2 = nn.Dropout(p=0.2)
+        self.dec_conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2)
+        self.dec_dropout3 = nn.Dropout(p=0.2)
+        self.dec_conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2)
+        self.dec_dropout4 = nn.Dropout(p=0.2)
+        self.dec_conv5 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2)
+        self.dec_dropout5 = nn.Dropout(p=0.2)
+        self.dec_conv6 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=2)
+        self.dec_dropout6 = nn.Dropout(p=0.2)
+        self.dec_conv7 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.dec_conv8 = nn.Conv2d(in_channels=64, out_channels=channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x) -> torch.Tensor:
+        # encoding
+        x = self.enc_conv1(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.enc_conv2(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.enc_conv3(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.enc_conv4(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.enc_conv5(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.enc_conv6(x)
+
+        # decoding
+        x = nn.functional.interpolate(x, size=3, mode="bilinear")
+        x = self.dec_conv1(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.dec_dropout1(x)
+        x = nn.functional.interpolate(x, size=8, mode="bilinear")
+        x = self.dec_conv2(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.dec_dropout2(x)
+        x = nn.functional.interpolate(x, size=15, mode="bilinear")
+        x = self.dec_conv3(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.dec_dropout3(x)
+        x = nn.functional.interpolate(x, size=32, mode="bilinear")
+        x = self.dec_conv4(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.dec_dropout4(x)
+        x = nn.functional.interpolate(x, size=63, mode="bilinear")
+        x = self.dec_conv5(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.dec_dropout5(x)
+        x = nn.functional.interpolate(x, size=127, mode="bilinear")
+        x = self.dec_conv6(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.dec_dropout6(x)
+        x = nn.functional.interpolate(x, size=64, mode="bilinear")
+        x = self.dec_conv7(x)
+        x = nn.functional.relu(x, inplace=True)
+        x = self.dec_conv8(x)
+
+        return x
--- train.py
+++ train.py
@@ -1,43 +1,73 @@
+import argparse
+import random
+
 import datasets
 import torch
-from torch.utils.data import DataLoader
-from torchvision import transforms
-import timm
-import models
 from PIL import Image
 from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+
+import models
+
 
 def common_preprocess(image: Image, device: torch.DeviceObjType) -> torch.Tensor:
     res = image.convert("RGB")
-    preprocess = transforms.Compose([
-        transforms.Resize((512, 512), antialias=True),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        transforms.RandomGrayscale(0.1),
-    ])
+    preprocess = transforms.Compose(
+        [
+            transforms.Resize((512, 512), antialias=True),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.RandomGrayscale(0.1),
+        ]
+    )
     res = preprocess(res).to(device)
     return res
 
+
 def resnet_preprocess(image: torch.Tensor) -> torch.Tensor:
-    preprocess = transforms.Compose([
-        transforms.Resize((512, 512), antialias=True),
-    ])
+    preprocess = transforms.Compose(
+        [
+            transforms.Resize((512, 512), antialias=True),
+        ]
+    )
     return preprocess(image)
 
-def pdn_preprocess(image: torch.Tensor) -> torch.Tensor:
-    preprocess = transforms.Compose([
-        transforms.Resize((256, 256), antialias=True),
-    ])
+
+def pdn_preprocess(image: torch.Tensor, resize_to: tuple[int, int]) -> torch.Tensor:
+    preprocess = transforms.Compose(
+        [
+            transforms.Resize(resize_to, antialias=True),
+        ]
+    )
     return preprocess(image)
+
+
+def distortion_preprocess(image: torch.Tensor) -> torch.Tensor:
+    distortion = random.choice([1, 2, 3])
+    factor = random.uniform(0.8, 1.2)
+    if distortion == 1:
+        distorted_image = transforms.functional.adjust_brightness(image, factor)
+    elif distortion == 2:
+        distorted_image = transforms.functional.adjust_contrast(image, factor)
+    elif distortion == 3:
+        distorted_image = transforms.functional.adjust_saturation(image, factor)
+
+    return distorted_image
+
 
 @torch.no_grad()
-def find_distribution(model: torch.nn.Module, feature_layer: int, dataset: datasets.IterableDatasetDict, device: torch.DeviceObjType, sample_size: int = 10_000) -> tuple:
+def find_distribution(
+    model: torch.nn.Module,
+    dataset: datasets.IterableDatasetDict,
+    device: torch.DeviceObjType,
+    sample_size: int = 10_000,
+) -> tuple:
     # find gaussian distribution of features
     features = None
     for data in dataset["train"].take(sample_size):
         image = common_preprocess(data["image"], device)
         image = resnet_preprocess(image).unsqueeze(0)
-        feature_map = model.forward(image)[feature_layer]
+        feature_map = model.forward(image)
         feature_map = torch.flatten(feature_map, start_dim=2)
         if features is None:
             features = feature_map
@@ -48,26 +78,31 @@ def find_distribution(model: torch.nn.Module, feature_layer: int, dataset: datas
     std, mean = torch.std_mean(features, dim=1)
     return std, mean
 
-def main():
-    channels = 100
-    epochs = 10_000
-    batch_size = 8
-    widresnet_feature_layer = 2
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    writer = SummaryWriter()
-
-    # if the dataset is gated/private, make sure you have run huggingface-cli login
-    dataset = datasets.load_dataset("imagenet-1k", trust_remote_code=True, streaming=True)
-
-    target_features_model = timm.create_model("wide_resnet101_2", features_only=True, pretrained=True).to(device)
+def train_teacher(
+    channels: int,
+    dataset: datasets.IterableDatasetDict,
+    device: torch.DeviceObjType,
+    epochs: int = 10_000,
+    batch_size: int = 8,
+    wideresnet_feature_layer: str = "layer1",
+    wideresnet_feature_layer_index: int = 1,
+    tensorboard_writer: SummaryWriter | None = None,
+) -> torch.nn.Module:
+    target_features_model = models.ChoppedWideResNet(
+        channels=channels, layer_to_extract_from=wideresnet_feature_layer, layer_index=wideresnet_feature_layer_index
+    ).to(device)
+    target_features_model.eval()
+    target_features_output_shape = target_features_model.forward(
+        resnet_preprocess(torch.rand([1, 3, 1, 1]).to(device))
+    ).shape
 
     print("finding normal distribution of features...")
-    std, mean = find_distribution(target_features_model, widresnet_feature_layer, dataset, device sample_size=100)
-    std = std.unsqueeze(1).unsqueeze(1).expand(-1, 64, 64)[:256,:,:]
-    mean = mean.unsqueeze(1).unsqueeze(1).expand(-1, 64, 64)[:256,:,:]
+    std, mean = find_distribution(target_features_model, dataset, device, sample_size=50)
+    std = std.unsqueeze(1).unsqueeze(1).expand(target_features_output_shape)
+    mean = mean.unsqueeze(1).unsqueeze(1).expand(target_features_output_shape)
 
-    teacher_pdn = models.get_pdn(channels=256).to(device)
+    teacher_pdn = models.PatchDescriptionNetwork(channels=channels).to(device)
     teacher_pdn.train()
 
     optimizer = torch.optim.Adam(teacher_pdn.parameters(), lr=1e-4, weight_decay=1e-5)
@@ -75,7 +110,6 @@ def main():
     print("starting training of teacher...")
     image_batch = None
     for data, iteration in zip(dataset["train"], range(batch_size * epochs)):
-
         image = common_preprocess(data["image"], device).unsqueeze(0)
         if image_batch is None:
             image_batch = image
@@ -85,15 +119,18 @@ def main():
         if (iteration + 1) % batch_size == 0:
             with torch.no_grad():
                 target_features = target_features_model.forward(resnet_preprocess(image_batch))
-                target_features = target_features[widresnet_feature_layer] # select features from layer 1
-                target_features = target_features[:,:256,:,:]
-                target_features = torch.sub(target_features, mean) / std
+                target_features = torch.sub(target_features, mean)
+                target_features = target_features / std
+                target_features = torch.nan_to_num(target_features)
 
-            predicted_features = teacher_pdn.forward(pdn_preprocess(image_batch))
+            predicted_features = teacher_pdn.forward(
+                pdn_preprocess(image_batch, (target_features_output_shape[2] * 4, target_features_output_shape[3] * 4))
+            )
 
-            loss = torch.mean((target_features - predicted_features)**2) # calculate mean square error
+            loss = torch.mean((target_features - predicted_features) ** 2)  # calculate mean square error
             print(f"epoch: {int(iteration/batch_size)}/{epochs}  loss: {loss.item()}")
-            writer.add_scalar('loss', loss.item(), int(iteration/batch_size))
+            if tensorboard_writer is not None:
+                tensorboard_writer.add_scalar("teacher training", loss.item(), int(iteration / batch_size))
 
             optimizer.zero_grad()
             loss.backward()
@@ -105,10 +142,140 @@ def main():
             torch.save(teacher_pdn, "models/tmp/teacher.pth")
 
     torch.save(teacher_pdn, "models/teacher.pth")
-    teacher_pdn.eval()
     print("finished training teacher!")
 
-    writer.close()
+
+def train_autoencoder(
+    channels: int,
+    dataset: datasets.IterableDatasetDict,
+    device: torch.DeviceObjType,
+    teacher: torch.nn.Module,
+    epochs: int = 10_000,
+    batch_size: int = 8,
+    tensorboard_writer: SummaryWriter | None = None,
+) -> torch.nn.Module:
+    autoencoder = models.AutoEncoder(channels=channels).to(device)
+    autoencoder.train()
+    teacher.eval()
+
+    optimizer = torch.optim.Adam(autoencoder.parameters(), lr=1e-4, weight_decay=1e-5)
+
+    for data, iteration in zip(dataset["train"], range(batch_size * epochs)):
+        image = common_preprocess(data["image"], device).unsqueeze(0)
+        image = distortion_preprocess(image)
+        if image_batch is None:
+            image_batch = image
+        else:
+            image_batch = torch.cat((image_batch, image), 0)
+
+        if (iteration + 1) % batch_size == 0:
+            with torch.no_grad():
+                teacher_result = teacher.forward(image_batch)
+            autoencoder_result = autoencoder.forward(image_batch)
+
+            loss = torch.mean((teacher_result - autoencoder_result) ** 2)
+            print(f"epoch: {int(iteration/batch_size)}/{epochs}  loss: {loss.item()}")
+            if tensorboard_writer is not None:
+                tensorboard_writer.add_scalar("autoencoder training", loss.item(), int(iteration / batch_size))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            image_batch = None
+
+
+def train_student(
+    channels: int,
+    dataset: datasets.IterableDatasetDict,
+    device: torch.DeviceObjType,
+    teacher: torch.nn.Module,
+    epochs: int = 10_000,
+    batch_size: int = 8,
+    tensorboard_writer: SummaryWriter | None = None,
+) -> torch.nn.Module:
+    student_pdn = models.PatchDescriptionNetwork(channels=channels * 2).to(device)
+    student_pdn.train()
+    teacher.eval()
+
+    optimizer = torch.optim.Adam(student_pdn.parameters(), lr=1e-4, weight_decay=1e-5)
+
+    for data, iteration in zip(dataset["train"], range(batch_size * epochs)):
+        image = common_preprocess(data["image"], device).unsqueeze(0)
+        if image_batch is None:
+            image_batch = image
+        else:
+            image_batch = torch.cat((image_batch, image), 0)
+
+        if (iteration + 1) % batch_size == 0:
+            teacher_result = teacher.forward(image_batch)
+            student_result = student_pdn.forward(image_batch)
+
+            loss = torch.mean((teacher_result - student_result) ** 2)
+            print(f"epoch: {int(iteration/batch_size)}/{epochs}  loss: {loss.item()}")
+            if tensorboard_writer is not None:
+                tensorboard_writer.add_scalar("student training", loss.item(), int(iteration / batch_size))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            image_batch = None
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--device", choices=["cpu", "cuda"])
+    parser.add_argument("--skip_teacher", action="store_true", default=False)
+    parser.add_argument("--skip_autoencoder", action="store_true", default=False)
+    parser.add_argument("--skip_student", action="store_true", default=False)
+    args = parser.parse_args()
+    if args.device is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+    else:
+        device = args.device
+
+    channels = 256
+
+    tensorboard_writer = SummaryWriter()
+
+    # if the dataset is gated/private, make sure you have run huggingface-cli login
+    generic_dataset = datasets.load_dataset("imagenet-1k", trust_remote_code=True, streaming=True)
+
+    if not args.skip_teacher:
+        teacher_pdn = train_teacher(
+            channels=channels,
+            dataset=generic_dataset,
+            device=device,
+            tensorboard_writer=tensorboard_writer,
+            wideresnet_feature_layer_index=0,
+        )
+
+    del generic_dataset
+    good_dataset = datasets.load_dataset(
+        "imagenet-1k", trust_remote_code=True, streaming=True
+    )  # change this to another appropriate dataset
+
+    if not args.skip_autoencoder:
+        autoencoder = train_autoencoder(
+            channels=channels,
+            dataset=good_dataset,
+            device=device,
+            teacher=teacher_pdn,
+            tensorboard_writer=tensorboard_writer,
+        )
+
+    if not args.skip_student:
+        student_pdn = train_student(
+            channels=channels,
+            dataset=good_dataset,
+            device=device,
+            teacher=teacher_pdn,
+            tensorboard_writer=tensorboard_writer,
+        )
+
+    tensorboard_writer.close()
+
 
 if __name__ == "__main__":
     main()
